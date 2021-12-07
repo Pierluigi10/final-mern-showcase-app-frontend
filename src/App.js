55fd3072bab00e7226da8cc3e69e9e94fc30f7e0
pierluigi <pierluigibaiano@gmail.com>
2021-12-07 14:47:12 +0100

    fix bug in handle_signupForm_signupButton

--- src/App.js
+++ src/App.js
@@ -66,6 +66,7 @@ function App() {
     };
     const response = await fetch("http://localhost:3003/login", requestOptions);
     const _currentUser = await response.json();
+    // console.log(_currentUser);
     setCurrentUser((prev) => ({ ...prev, ..._currentUser }));
   };
 
@@ -86,64 +87,64 @@ function App() {
     }
   };
 
+  // SIGNUP FORM FIELD HANDLERS
+  const handle_signupFormField_login = (e) => {
+    let login = e.target.value;
+    setSignupFormField_login(login);
+  };
+  const handle_signupFormField_password1 = (e) => {
+    let password1 = e.target.value;
+    setSignupFormField_password1(password1);
+  };
+  const handle_signupFormField_password2 = (e) => {
+    let password2 = e.target.value;
+    setSignupFormField_password2(password2);
+  };
+  const handle_signupFormField_firstName = (e) => {
+    let firstName = e.target.value;
+    setSignupFormField_firstName(firstName);
+  };
+  const handle_signupFormField_lastName = (e) => {
+    let lastName = e.target.value;
+    setSignupFormField_lastName(lastName);
+  };
+  const handle_signupFormField_email = (e) => {
+    let email = e.target.value;
+    setSignupFormField_email(email);
+  };
 
-
-	// SIGNUP FORM FIELD HANDLERS
-	const handle_signupFormField_login = (e) => {
-		let login = e.target.value;
-		setSignupFormField_login(login);
-	}
-	const handle_signupFormField_password1 = (e) => {
-		let password1 = e.target.value;
-		setSignupFormField_password1(password1);
-	}
-	const handle_signupFormField_password2 = (e) => {
-		let password2 = e.target.value;
-		setSignupFormField_password2(password2);
-	}
-	const handle_signupFormField_firstName = (e) => {
-		let firstName = e.target.value;
-		setSignupFormField_firstName(firstName);
-	}
-	const handle_signupFormField_lastName = (e) => {
-		let lastName = e.target.value;
-		setSignupFormField_lastName(lastName);
-	}
-	const handle_signupFormField_email = (e) => {
-		let email = e.target.value;
-		setSignupFormField_email(email);
-	}
-	const handle_signupForm_signupButton = async (e) => {
-		e.preventDefault();
-		const requestOptions = {
-			method: 'POST',
-			credentials: "include",
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify(
-				{
-					login: signupFormField_login,
-					password1: signupFormField_password1,
-					password2: signupFormField_password2,
-					firstName: signupFormField_firstName,
-					lastName: signupFormField_lastName,
-					email: signupFormField_email,
-				}
-			),
-		};
-		const response = await fetch('http://localhost:3003/signup', requestOptions);
-		if (response.ok) {
-			const _currentUser = await response.json();
-			setCurrentUser(prev => ({ ...prev, ..._currentUser }));
-			setSignupFormField_login('');
-			setSignupFormField_password1('');
-			setSignupFormField_password2('');
-			setSignupFormField_firstName('');
-			setSignupFormField_lastName('');
-			setSignupFormField_email('');
-		}
-	}
-
-
+  const handle_signupForm_signupButton = async (e) => {
+    e.preventDefault();
+    const requestOptions = {
+      method: "POST",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        user: {
+          login: signupFormField_login,
+          password1: signupFormField_password1,
+          password2: signupFormField_password2,
+          firstName: signupFormField_firstName,
+          lastName: signupFormField_lastName,
+          email: signupFormField_email,
+        },
+      }),
+    };
+    const response = await fetch(
+      "http://localhost:3003/signup",
+      requestOptions
+    );
+    if (response.ok) {
+      const _currentUser = await response.json();
+      setCurrentUser((prev) => ({ ...prev, ..._currentUser }));
+      setSignupFormField_login("");
+      setSignupFormField_password1("");
+      setSignupFormField_password2("");
+      setSignupFormField_firstName("");
+      setSignupFormField_lastName("");
+      setSignupFormField_email("");
+    }
+  };
 
   return (
     <div className="App">
