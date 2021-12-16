e2b8377476c90d4bd387ef5503e3d431ccd47362
pierluigi <pierluigibaiano@gmail.com>
2021-12-16 08:52:06 +0100

    se welcome page

--- src/pages/PageWelcome.js
+++ src/pages/PageWelcome.js
@@ -3,12 +3,19 @@ import AppContext from "../AppContext";
 
 const PageWelcome = () => {
   const { currentUser } = useContext(AppContext);
+  console.log(currentUser);
   return (
     <div>
+     
       <h1>FINAL MERN Showcase App</h1>
-      <h2>
+      {/* <h2>
         Current User: {currentUser.firstName} {currentUser.lastName}
-      </h2>
+      </h2> */}
+      {currentUser.login !== 'anonymousUser' && (
+          <>
+            <h2>Welcome {currentUser.login}</h2>
+          </>
+        )}
     </div>
   );
 };
